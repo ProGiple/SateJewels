32691c983570a58989ccfcc380bb4e40f62dbfb7
NovaSparkle <novasparkly@gmail.com>
2025-01-07 21:48:51 +0500

    1.1.1

--- src/main/java/org/satellite/progiple/satejewels/SateJewels.java
+++ src/main/java/org/satellite/progiple/satejewels/SateJewels.java
@@ -3,8 +3,6 @@ package org.satellite.progiple.satejewels;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.satellite.progiple.satejewels.other.Placeholders;
-import org.satellite.progiple.satejewels.other.configs.Config;
-import org.satellite.progiple.satejewels.other.configs.DataConfig;
 
 import java.util.Objects;
 
@@ -16,9 +14,6 @@ public final class SateJewels extends JavaPlugin {
     public void onEnable() {
         plugin = this;
         saveDefaultConfig();
-        new DataConfig(plugin.getDataFolder());
-        new Config();
-
         Command command = new Command();
         Objects.requireNonNull(getCommand("satejewels")).setTabCompleter(command);
         Objects.requireNonNull(getCommand("satejewels")).setExecutor(command);
