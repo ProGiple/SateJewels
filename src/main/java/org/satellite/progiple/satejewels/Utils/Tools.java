d8b1eaf28a577e025aeb3e9a29b79e28fd5380d9
ProGiple <ghosthostgtop@gmail.com>
2025-03-11 13:09:43 +0300

    2.0.3.4

--- src/main/java/org/satellite/progiple/satejewels/Utils/Tools.java
+++ src/main/java/org/satellite/progiple/satejewels/Utils/Tools.java
@@ -1,14 +1,9 @@
 package org.satellite.progiple.satejewels.Utils;
 
-import org.black_ixx.playerpoints.PlayerPoints;
-import org.black_ixx.playerpoints.PlayerPointsAPI;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.satellite.progiple.satejewels.SateJewels;
-import org.satellite.progiple.satejewels.api.SJAPI;
 import org.satellite.progiple.satejewels.storages.configs.managers.ConfigManager;
 
 import java.util.Map;
@@ -38,17 +33,6 @@ public class Tools {
         if (sender instanceof Player player) {
             player.playSound(player.getLocation(), ConfigManager.getSound(soundId), 1, 1);
         }
-    }
-
-    public static void syncSJtoPP(String nick) {
-        if (!ConfigManager.getBool("jewelsSettings.playerPointsAutoTransfer") ||
-                Bukkit.getServer().getPluginManager().getPlugin("PlayerPoints") == null) return;
-        PlayerPointsAPI ppapi = PlayerPoints.getInstance().getAPI();
-        if (ppapi == null) return;
-
-        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayerIfCached(nick);
-        if (offlinePlayer == null) return;
-        ppapi.set(offlinePlayer.getUniqueId(), SateJewels.getPlugin().getSjapi().getJewels(nick));
     }
 
     public static int toInt(String value) {
